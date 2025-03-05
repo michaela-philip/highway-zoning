b9e5cf0c5c63b29128e016181d632165730e0f21
Michaela <michaela.philip@emory.edu>
2025-03-05 15:42:52 -0500

    code works

--- data_code/import_census.py
+++ data_code/import_census.py
@@ -3,12 +3,13 @@ import pandas as pd
 census = '/homes/nber/philipm/bulk/jones_ipums549/census-ipums/v2024/csv/1950.csv'
 
 chunk_counter = 0
-
-for chunk in pd.read_csv(census, chunksize = 1000000):
+# remember that this appends - if you need to rerun this code delete the existing csv first
+for chunk in pd.read_csv(census, chunksize=1000000, low_memory=False):
     chunk = chunk[chunk['statefip'] == 13]
-    chunk.to_csv('data/output/census_ga.csv', mode='a', header = (chunk_counter == 0), index = False)
-    chunk_counter += 1
-    print(f'Chunk {chunk_counter}')
-    del chunk
+    
+    if not chunk.empty:  
+        chunk.to_csv('data/output/census_ga.csv', mode='a', header=(chunk_counter == 0), index=False)
+        chunk_counter += 1
+        print(f'Chunk {chunk_counter} processed')
 
-print("GA complete")
+print("Processing complete!")
