1a78400341401b7303ceb58fa5676e1ec160dd2f
Michaela <michaela.philip@emory.edu>
2025-03-14 14:39:21 -0400

    change to 1940 census

--- data_code/import_census.py
+++ data_code/import_census.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-census = '/homes/nber/philipm/bulk/jones_ipums549/census-ipums/v2024/csv/1950.csv'
+census = '/homes/nber/philipm/bulk/jones_ipums549/census-ipums/current/csv/1940.csv'
 
 chunk_counter = 0
 # remember that this appends - if you need to rerun this code delete the existing csv first
@@ -8,7 +8,7 @@ for chunk in pd.read_csv(census, chunksize=1000000, low_memory=False):
     chunk = chunk[chunk['statefip'] == 13]
     
     if not chunk.empty:  
-        chunk.to_csv('data/output/census_ga.csv', mode='a', header=(chunk_counter == 0), index=False)
+        chunk.to_csv('data/output/census_ga_1940.csv', mode='a', header=(chunk_counter == 0), index=False)
         chunk_counter += 1
         print(f'Chunk {chunk_counter} processed')
 
