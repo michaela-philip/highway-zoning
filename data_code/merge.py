8fd9a99ae117992d31b2d12d877d7db360c4d9f6
michaela-philip <michaela.philip@emory.edu>
2025-03-19 17:00:34 -0400

    change hwy crs and add in atl zoning map

--- data_code/merge.py
+++ data_code/merge.py
@@ -8,6 +8,11 @@ interstates = gpd.read_file('data/input/shapefiles/1960/interstates1959_del.shp'
 state_paved = gpd.read_file('data/input/shapefiles/1960/stateHighwayPaved1959_del.shp')
 us_paved = gpd.read_file('data/input/shapefiles/1960/usHighwayPaved1959_del.shp')
 
+# change crs to ESPG 3857
+interstates = interstates.to_crs('EPSG:3857')
+state_paved = state_paved.to_crs('EPSG:3857')
+us_paved = us_paved.to_crs('EPSG:3857')
+
 all_roads = gpd.GeoDataFrame()
 
 # fclass information from Baik et al. (2010)
@@ -24,5 +29,14 @@ us_paved['designation'] = 'us_paved'
 all_roads = pd.concat([interstates, state_paved, us_paved])
 print('concat done', all_roads.shape, all_roads.columns)
 
-all_roads.plot('designation')
+# read in atlanta zoning map
+atl_zone = gpd.read_file('data/input/zoning_shapefiles/atlanta/zoning.shp')
+atl_zone['zoning'] = np.where(atl_zone['Zonetype'] == 'dwelling', 'residential', 
+                              np.where(atl_zone['Zonetype'] == 'apartment', 'residential', 
+                                       np.where(atl_zone['Zonetype'] == 'industrial', 'industrial', 
+                                                np.where(atl_zone['Zonetype'] == 'business', 'industrial', 'nan'))))
+
+# spatial join
+atl_hwy = gpd.sjoin(atl_zone, all_roads, how = 'left', predicate = 'overlaps')
+atl_hwy.plot(column = 'Zonetype', legend = True)
 plt.show()
