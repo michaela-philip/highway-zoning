d85fca15e02095a267a40d55b582314c9f04e674
michaela-philip <michaela.philip@emory.edu>
2025-04-28 16:01:10 -0400

    read in street list

--- data_code/clean_ga.py
+++ data_code/clean_ga.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import geopandas as gpd
 
-ga = pd.read_csv('data/output/census_ga.csv')
+# ga = pd.read_csv('data/output/census_ga.csv')
+from scrape_streets import street_list
+print(street_list)
 
 # pull all observations in ATL - city code 0350
-atl = ga[ga['city'] == 350]
+# atl = ga[ga['city'] == 350]
