c37a2adb83d257bec434702a60206778e1417c4e
michaela-philip <michaela.philip@emory.edu>
2025-05-30 13:29:41 -0400

    clean and interpolate street addresses

--- data_code/clean_ga.py
+++ data_code/clean_ga.py
@@ -1,9 +1,71 @@
 import pandas as pd
 import geopandas as gpd
+import numpy as np
 
-# ga = pd.read_csv('data/output/census_ga.csv')
 from scrape_streets import street_list
-print(street_list)
 
-# pull all observations in ATL - city code 0350
-# atl = ga[ga['city'] == 350]
+ga = pd.read_csv('data/output/census_ga_1940.csv')
+
+# keep only columns and counties we need
+cols = ['valueh', 'race', 'street', 'city', 'urban', 'countyicp', 'stateicp', 'rent', 
+        'enumdist', 'respond', 'numperhh', 'numprec', 'serial', 'rawhn', 'ownershp', 'pageno']
+ga2 = ga.loc[ga['countyicp'].isin([1210, 890]), cols]
+atl = ga2[ga2['city'] == 350].copy()
+
+# recode valueh and rent missing values
+atl['valueh'] = atl['valueh'].replace([9999998, 9999999], np.nan)
+atl['rent'] = atl['rent'].replace([0, 9998, 9999], np.nan)
+
+# keep one observation per household/serial 
+atl = atl.drop_duplicates(subset = 'serial', keep = 'first')
+
+# function to clean and standardize addresses
+def clean_addresses(df):
+    # extract any additional information in () from house number
+    df['street'] = df['street'].astype(str)
+    df[['street', 'streetinfo']] = df['street'].str.extract(r'\s*([^\(]+)\s*(?:\(([^)]*)\))?\s*')
+
+    # identify rawhn entries that are likely to be hotels and recode as null
+    pattern = r'([0-9][ ][a-zA-Z])|([0-9][-][a-zA-Z])|([a-zA-Z][ ][0-9])|([a-zA-Z][-][0-9])'
+    df.loc[df['rawhn'].str.contains(pattern, na=False), 'rawhn'] = np.nan
+
+    # extract any non-numeric characters from rawhn and make it numeric
+    df[['rawhn', 'rawhninfo']] = df['rawhn'].str.extract(r'(\d+)\s*([^\d]*)')    
+    df['rawhn'] = pd.to_numeric(df['rawhn'], errors='coerce')
+    
+    # get previous entry's values to fill in missing streets where appropriate
+    prev_rawhn = df['rawhn'].shift(1)
+    prev_street = df['street'].shift(1)
+    prev_page = df['pageno'].shift(1)
+
+    ## interpolate missing address values ##
+    # fill in missing street as previous street if certain conditions are met
+    mask = (
+        df['street'].isna() &
+        df['rawhn'].notna() &
+        prev_rawhn.notna() &
+        (prev_page == df['pageno']) & 
+        ((prev_rawhn - df['rawhn']).abs() <= 6) # norm from PVC/Logan and Zhang (2019)
+    )
+    df.loc[mask, 'street'] = prev_street[mask]
+
+    # interpolate missing house numbers for renters - use previous house number
+    house_mask_rent = (
+        df['rawhn'].isna() &
+        df['street'].notna() & 
+        df['tenure'] != 10 &
+        (prev_street == df['street'])
+    )
+    df.loc[house_mask_rent, 'rawhn'] = prev_rawhn[house_mask_rent]
+    
+    # interpolate missing house numbers for owners - add 2 to previous house number
+    house_mask_own = (
+        df['rawhn'].isna() &
+        df['street'].notna() & 
+        df['tenure'] == 10 &
+        (prev_street == df['street'])
+    )
+    df.loc[house_mask_own, 'rawhn'] = prev_rawhn[house_mask_own] + 2
+    return df
+
+atl = clean_addresses(atl)
