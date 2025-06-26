13b0bb7b1f2b7389aa8993cd764726e18c70d39b
Michaela <michaela.philip@emory.edu>
2025-06-26 17:54:21 -0400

    pickle census

--- data_code/import_census.py
+++ data_code/import_census.py
@@ -2,14 +2,26 @@ import pandas as pd
 
 census = '/homes/nber/philipm/bulk/jones_ipums549/census-ipums/current/csv/1940.csv'
 
-chunk_counter = 0
+#chunk_counter = 0
 # remember that this appends - if you need to rerun this code delete the existing csv first
+#for chunk in pd.read_csv(census, chunksize=1000000, low_memory=False):
+    #chunk = chunk[chunk['statefip'] == 13]
+    
+    #if not chunk.empty:  
+        #chunk.to_csv('data/output/census_ga_1940.csv', mode='a', header=(chunk_counter == 0), index=False)
+        #chunk_counter += 1
+        #print(f'Chunk {chunk_counter} processed')
+
+#print("Processing complete!")
+
+chunks = []
 for chunk in pd.read_csv(census, chunksize=1000000, low_memory=False):
     chunk = chunk[chunk['statefip'] == 13]
-    
-    if not chunk.empty:  
-        chunk.to_csv('data/output/census_ga_1940.csv', mode='a', header=(chunk_counter == 0), index=False)
-        chunk_counter += 1
-        print(f'Chunk {chunk_counter} processed')
+    if not chunk.empty:
+        chunks.append(chunk)
+        print(f'Chunk with {len(chunk)} rows processed')
 
-print("Processing complete!")
+# Concatenate all chunks into a single DataFrame
+df = pd.concat(chunks, ignore_index=True)
+df.to_pickle('data/input/ga_1940.pkl')
+print("Processing complete and pickled!")
