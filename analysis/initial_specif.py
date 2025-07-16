181c246f0dcfd76064ca5c6e2ea13ba857ae9c43
michaela-philip <michaela.philip@emory.edu>
2025-07-16 12:32:20 -0400

    first run with mblack >= 50% and report in separate table, then run alternate definitions and report in new table

--- analysis/initial_specif.py
+++ analysis/initial_specif.py
@@ -4,15 +4,37 @@ import statsmodels.formula.api as smf
 
 atl_sample = pd.read_pickle('data/output/atl_sample.pkl')
 
+model_50_pct = 'hwy ~ mblack_50_pct + Residential + (mblack_50_pct * Residential) + rent + valueh + distance_to_cbd'
 model_pct = 'hwy ~ mblack_mean_pct + Residential + (mblack_mean_pct * Residential) + rent + valueh + distance_to_cbd'
 model_share = 'hwy ~ mblack_mean_share + Residential + (mblack_mean_share * Residential) + rent + valueh + distance_to_cbd'
 
+results_50_pct = smf.ols(model_50_pct, data=atl_sample).fit(cov_type='HC3')
 results_pct = smf.ols(model_pct, data=atl_sample).fit(cov_type='HC3')
 results_share = smf.ols(model_share, data=atl_sample).fit(cov_type='HC3')
 
+# initial results with >= 50% black population threshold
+results = results_50_pct.summary2(title = 'Mean 50% Threshold').tables[1]
+results = results.T.rename(columns={
+    'Rent': 'rent',
+    'Home Value': 'valueh',
+    'Highway': 'hwy',
+    'mblack_50_pct': 'Majority Black (50% Threshold)',
+    'mblack_50_pct:Residential': 'Majority Black (50% Threshold) x Residential',
+    'Distance to CBD': 'distance_to_cbd'
+}).T
+print(results)
+
+results.style.format(precision=3).to_latex('tables/initial_results.tex',
+                 column_format='lcccccc', 
+                 caption = 'Initial Results',
+                 label = 'tab:initial_results',
+                 position = 'h',
+                 position_float = 'centering',
+                 hrules = True)
+
+# second results with alt. definitions of majority Black
 results = pd.concat([results_pct.summary2(title = 'Mean Percentage').tables[1],
                      results_share.summary2(title = 'Mean Share').tables[1]])
-
 results = results.T.rename(columns={
     'rent': 'Rent',
     'valueh': 'Home Value',
@@ -23,13 +45,12 @@ results = results.T.rename(columns={
     'mblack_mean_share:Residential':'Majority Black (Share) x Residential',
     'distance_to_cbd' : 'Distance to CBD'
 }).T
-
 print(results)
 
-results.style.format(precision=3).to_latex('tables/initial_results.tex',
+results.style.format(precision=3).to_latex('tables/second_results.tex',
                  column_format='lcccccc', 
-                 caption = 'Initial Results',
-                 label = 'tab:initial_results',
+                 caption = 'Alternate Definitions of Majority Black',
+                 label = 'tab:second_results',
                  position = 'h',
                  position_float = 'centering',
                  hrules = True)
