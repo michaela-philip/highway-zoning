7e356b8a372cca12e35fa70c4093fc9ca963c455
Michaela <michaela.philip@emory.edu>
2025-08-13 13:28:14 -0400

    formatting with std below mean

--- analysis/summary_stats.py
+++ analysis/summary_stats.py
@@ -3,9 +3,8 @@ import numpy as np
 import re
 
 # function to export df as latex table with full page width and add'l formatting
-def export_latex_table(df, caption, label):
-    df['Mean'] = df.apply(
-        lambda row: f"\\makecell[tr]{{{row['Mean']} \\\\ ({row['Std']:.3f})}}", axis=1)
+def export_latex_table(df, columns, caption, label):
+    df = df[columns]
     num_cols = df.shape[1]
     col_format = '@{\\extracolsep{\\fill}}l*' + f'{{{num_cols}}}' + '{r}'
     text = df.style.format(precision=2).to_latex(position_float = 'centering',
@@ -38,35 +37,32 @@ rows = ['Residents', 'Households', 'Median Rent', 'Median Home Value', 'Percent 
 sum_stats = pd.DataFrame({
     'Mean': atl_sample[rows].mean(),
     'Std': atl_sample[rows].std(),
+    'Min':atl_sample[rows].min(),
+    'Max':atl_sample[rows].max(),
     'N': atl_sample[rows].count(),
 })
+columns = ['Mean', 'Min', 'Max', 'N']
 
-columns = ['Mean', 'Std', 'Min', 'Max', 'N']
-sum_stats = sum_stats[columns]
-
-print(sum_stats)
-export_latex_table(sum_stats, caption = 'Sample Grid Summary Statistics', label = 'tab:summary_stats')
+sum_stats['Mean'] = sum_stats.apply(
+    lambda row: f"\\makecell[tr]{{{row['Mean']} \\\\ ({row['Std']:.3f})}}", axis=1)
+export_latex_table(sum_stats, columns = columns, caption = 'Sample Grid Summary Statistics', label = 'tab:summary_stats')
 
 # summary statistics by zoning designation
 rows = ['Residents', 'Households', 'Median Rent', 'Median Home Value', 
         'Percent Black', 'Highway Present (1940)', 'Highway Present (1959)', 'Highway Constructed (1940-1959)']
 
-# agg_funcs = {
-#     'Residents':'mean',
-#     'Households': 'mean',
-#     'Median Rent' : 'mean',
-#     'Median Home Value': 'mean',
-#     'Percent Black': 'mean',
-#     'Highway Present':'mean'
-# }
-
-zoning_sum = atl_sample.groupby('Residential')[rows].agg(['mean', 'std']).T
+zoning_mean = atl_sample.groupby('Residential')[rows].agg('mean')
+zoning_std = atl_sample.groupby('Residential')[rows].agg('std')
 zoning_stats = pd.DataFrame({
-    'Industrial': zoning_sum[0],
-    'Residential':zoning_sum[1]
+    'Industrial': zoning_mean.T[0],
+    'std_i':zoning_std.T[0],
+    'Residential':zoning_mean.T[1],
+    'std_r':zoning_std.T[1]
 })
 columns = ['Industrial', 'Residential']
-zoning_stats = zoning_stats[columns]
 
-print(zoning_stats)
-export_latex_table(zoning_stats, caption = 'Summary Statistics by Zoning Designation', label = 'tab:summary_stats_zone')
+zoning_stats['Industrial'] = zoning_stats.apply(
+    lambda row: f"\\makecell[tr]{{{row['Industrial']} \\\\ ({row['std_i']:.3f})}}", axis=1)
+zoning_stats['Residential'] = zoning_stats.apply(
+    lambda row: f"\\makecell[tr]{{{row['Residential']} \\\\ ({row['std_r']:.3f})}}", axis=1)
+export_latex_table(zoning_stats, columns = columns, caption = 'Summary Statistics by Zoning Designation', label = 'tab:summary_stats_zone')
