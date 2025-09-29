cb99a257c4802b0721a000e47b9f8614e1e50499
michaela-philip <michaela.philip@emory.edu>
2025-09-29 15:11:22 -0400

    geocode in 4 chunks

--- data_code/clean.py
+++ data_code/clean.py
@@ -312,17 +312,20 @@ def geocode_addresses(df_orig, city_sample):
 
     # geocode using censusbatchgeocoder
     print('starting geocoding')
-    third1, third2= int(len(df)/3), (2 * int(len(df)/3))
-    d1 = df.iloc[:third1]
-    d2 = df.iloc[(third1+1):(third2)]
-    d3 = df.iloc[(third2 + 1):]
+    fourth1, fourth2, fourth3= int(len(df)/3), (2 * int(len(df)/3)), (3 * int(len(df)/3))
+    d1 = df.iloc[:fourth1]
+    d2 = df.iloc[(fourth1+1):(fourth2)]
+    d3 = df.iloc[(fourth2 + 1):(fourth3)]
+    d4 = df.iloc[(fourth3 + 1):]
     result1 = pd.DataFrame(censusbatchgeocoder.geocode(d1.to_dict(orient = 'records'), zipcode = None))
     print('first done')
     result2 = pd.DataFrame(censusbatchgeocoder.geocode(d2.to_dict(orient = 'records'), zipcode = None))
     print('second done')
     result3 = pd.DataFrame(censusbatchgeocoder.geocode(d3.to_dict(orient = 'records'), zipcode = None))
     print('third done')
-    geocoded_df = pd.concat([result1, result2, result3])
+    result4 = pd.DataFrame(censusbatchgeocoder.geocode(d4.to_dict(orient = 'records'), zipcode = None))
+    print('fourth done')
+    geocoded_df = pd.concat([result1, result2, result3, result4])
     geocoded_df.to_pickle('data/intermed/atl_geocoded.pkl')
     print(f"{geocoded_df['is_exact'].notna().sum()} records geocoded")
     print(f"{(geocoded_df['is_match'] == 'Tie').sum()} ties")
