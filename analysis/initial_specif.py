31011c61ad6e944a10932d157c2d855a4f3af3c4
Michaela <michaela.philip@emory.edu>
2025-11-03 11:15:41 -0500

    correctly import candidate_dict

--- analysis/initial_specif.py
+++ analysis/initial_specif.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -6,11 +9,11 @@ import numpy as np
 from helpers.latex_formatting import export_single_regression, export_multiple_regressions, format_regression_results
 
 df = pd.read_pickle('data/output/sample.pkl')
-from data_code.candidates import candidates_dict
+from data_code.candidates import candidate_dict
 
 out_frames = []
 for city in df['city'].unique():
-    candidates = candidates_dict[city]
+    candidates = candidate_dict[city]
     controls = df.loc[(df['city'] == city) & (~df['grid_id'].isin(candidates))].copy()
     treated = df.loc[(df['city'] == city) & (df['hwy']==1)].copy()
     out_frames.append(controls)
@@ -18,16 +21,16 @@ for city in df['city'].unique():
     
 sample = pd.concat(out_frames, ignore_index=True)
 
-model_naive = 'hwy ~ mblack_1945def + Residential + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy'
+model_naive = 'hwy ~ mblack_1945def + Residential + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy + C(city)'
 results_naive = format_regression_results(smf.ols(model_naive, data=sample).fit(cov_type='HC3'))
 
-model_1945def = 'hwy ~ mblack_1945def + Residential + (mblack_1945def * Residential) + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy'
+model_1945def = 'hwy ~ mblack_1945def + Residential + (mblack_1945def * Residential) + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy + C(city)'
 results_1945def = format_regression_results(smf.ols(model_1945def, data=sample).fit(cov_type='HC3'))
 
-model_pct = 'hwy ~ mblack_mean_pct + Residential + (mblack_mean_pct * Residential) + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy'
+model_pct = 'hwy ~ mblack_mean_pct + Residential + (mblack_mean_pct * Residential) + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy + C(city)'
 results_pct = format_regression_results(smf.ols(model_pct, data=sample).fit(cov_type='HC3'))
 
-model_share = 'hwy ~ mblack_mean_share + Residential + (mblack_mean_share * Residential) + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy'
+model_share = 'hwy ~ mblack_mean_share + Residential + (mblack_mean_share * Residential) + np.log(rent) + np.log(valueh) + distance_to_cbd + dist_to_hwy + C(city)'
 results_share = format_regression_results(smf.ols(model_share, data=sample).fit(cov_type='HC3'))
 
 # naive regression in its own table
