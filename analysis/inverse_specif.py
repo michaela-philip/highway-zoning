6ccb509e7b736e70b5716e4677224c6f080403fb
Michaela <michaela.philip@emory.edu>
2025-11-13 15:58:40 -0500

    only 1945 def, only inverse candidates

--- analysis/inverse_specif.py
+++ analysis/inverse_specif.py
@@ -18,24 +18,10 @@ out_frames = []
 for city in df['city'].unique():
     candidates = candidate_dict[city]
     controls = df.loc[(df['city'] == city) & (~df['grid_id'].isin(candidates))].copy()
-    treated = df.loc[(df['city'] == city) & (df['hwy']==1) & (~df['grid_id'].isin(candidates))].copy()
     out_frames.append(controls)
-    out_frames.append(treated)
 sample = pd.concat(out_frames, ignore_index=True)
 
 model_1945def = 'hwy ~ (mblack_1945def * Residential) + np.log(rent) + np.log(valueh) + dist_water + owner + C(city)'
 results_1945def = format_regression_results(smf.ols(model_1945def, data=sample).fit(cov_type='cluster', cov_kwds={'groups': sample['city']}))
-
-model_pct = 'hwy ~ mblack_mean_pct + Residential + (mblack_mean_pct * Residential) + np.log(rent) + np.log(valueh) + dist_water + owner + C(city)'
-results_pct = format_regression_results(smf.ols(model_pct, data=sample).fit(cov_type='cluster', cov_kwds={'groups': sample['city']}))
-
-model_share = 'hwy ~ mblack_mean_share + Residential + (mblack_mean_share * Residential) + np.log(rent) + np.log(valueh) + dist_water + owner + C(city)'
-results_share = format_regression_results(smf.ols(model_share, data=sample).fit(cov_type='cluster', cov_kwds={'groups': sample['city']}))
-
-# other results together ?
-# export_multiple_regressions([results_1945def, results_pct, results_share],
-#                             caption = 'Determinants of Highway Placement',
-#                             label = 'tab:inverse_results',
-#                             leaveout = ['dist_water', 'owner'])
 
 export_single_regression(results_1945def, caption = 'Determinants of Highway Placement', label = 'tab:inverse_specif', widthmultiplier = 0.7, leaveout = ['dist_water', 'owner'])
